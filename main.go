b0b7e561b41cb1a7bda0d19ebbc4497ce46e96fe
robcrockbb <robert@vizsimply.com>
2023-01-04 18:44:30 -0300

    Created getCutomer handler to allow users to get a single customer.

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
-	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
 type customer struct {
-	ID uuid.UUID
+	ID int
 	Name string
 	Role string
 	Email string
@@ -18,9 +21,8 @@ type customer struct {
 var customers []customer
 
 func seedCustomers() {
-
 	c1 := customer{
-			ID: uuid.New(),
+			ID: 0,
 			Name: "Robert",
 			Role: "customer",
 			Email: "r@g.com",
@@ -28,7 +30,7 @@ func seedCustomers() {
 			Contacted: false,
 		}
 	c2 := customer{
-			ID: uuid.New(),
+			ID: 1,
 			Name: "Robert",
 			Role: "customer",
 			Email: "r@g.com",
@@ -36,7 +38,7 @@ func seedCustomers() {
 			Contacted: false,
 		}
 	c3 := customer{
-			ID: uuid.New(),
+			ID: 2,
 			Name: "Robert",
 			Role: "customer",
 			Email: "r@g.com",
@@ -45,11 +47,61 @@ func seedCustomers() {
 		}
 
 	customers = append(customers, c1, c2, c3)
+}
+
+// Getting a single customer through a /customers/{id} path
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	id := mux.Vars(r)["id"]
+
+	for k := range customers {
+		if fmt.Sprint(k) == id {
+			json.NewEncoder(w).Encode(customers[k])
+		}
+	}
 
 }
+
+// Getting all customers through a the /customers path
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(customers)
+
+}
+
+// Creating a customer through a /customers path
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	var newEntry = customer{}
+
+	// Read the HTTP request body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	// Encode the request body
+	json.Unmarshal(reqBody, &newEntry)
+
+	append(customers, newEntry)
+
+	json.NewEncoder(w).Encode(customers)
+
+}
+
+// Updating a customer through a /customers/{id} path
+// Deleting a customer through a /customers/{id} path
 
 func main() {
 	seedCustomers()
 
-	fmt.Println(customers)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/customers", getCustomers).Methods("GET")
+	router.HandleFunc("/customer/{id}", getCustomer).Methods("GET")
+
+	fmt.Println("Server is starting on port 3000")
+	http.ListenAndServe(":3000", router)
 }
