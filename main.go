e9bfe65eca1a13418d52e689097a20c339afc32c
robcrockbb <robert@vizsimply.com>
2023-01-08 16:03:35 -0300

    Responded with an HTML template instead of several seperate print lines.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -50,6 +51,17 @@ func seedCustomers() {
 	customers = append(customers, c1, c2, c3)
 }
 
+// The home route is a client API endpoint, and includes a brief overview of the API (e.g., available endpoints).
+func getAPIDescription(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	t, _  := template.ParseFiles("index.html")
+
+	t.Execute(w, nil)
+
+}
+
 // Getting a single customer through a /customers/{id} path
 func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -75,7 +87,7 @@ func getCustomers(w http.ResponseWriter, r *http.Request) {
 }
 
 // Creating a customer through a /customers path
-func createCustomer(w http.ResponseWriter, r *http.Request) {
+func addCustomer(w http.ResponseWriter, r *http.Request) {
 	// Set the appropriate Content-Type in the response header
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
@@ -121,17 +133,44 @@ func updateCustomer(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(customers)
 
 }
+
 // Deleting a customer through a /customers/{id} path
+func deleteCustomer(w http.ResponseWriter, r *http.Request) {
+	// Set the appropriate Content-Type in the response header
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	id, _ := strconv.Atoi(mux.Vars(r)["id"])
+	nullReplacement := customer{
+			ID: 0,
+			Name: "",
+			Role: "",
+			Email: "",
+			Phone: "",
+			Contacted: false,
+    }
+
+	// Remove the element at index i from a.
+	customers[id] = customers[len(customers)-1] // Copy last element to index i.
+	customers[len(customers)-1] = nullReplacement // Erase last element (write zero value).
+	customers = customers[:len(customers)-1] // Truncate slice.
+
+	// Regardless of successful resource creation or not, return the current state of the "dictionary" map
+	json.NewEncoder(w).Encode(customers)
+
+}
 
 func main() {
 	seedCustomers()
 
 	router := mux.NewRouter()
 
+	router.HandleFunc("/", getAPIDescription).Methods("GET")
+	router.HandleFunc("/customers/{id}", getCustomer).Methods("GET")
 	router.HandleFunc("/customers", getCustomers).Methods("GET")
-	router.HandleFunc("/customers", createCustomer).Methods("POST")
-	router.HandleFunc("/customer/{id}", getCustomer).Methods("GET")
+	router.HandleFunc("/customers", addCustomer).Methods("POST")
 	router.HandleFunc("/customer/{id}", updateCustomer).Methods("PATCH")
+	router.HandleFunc("/customer/{id}", deleteCustomer).Methods("DELETE")
 
 	fmt.Println("Server is starting on port 3000")
 	http.ListenAndServe(":3000", router)
